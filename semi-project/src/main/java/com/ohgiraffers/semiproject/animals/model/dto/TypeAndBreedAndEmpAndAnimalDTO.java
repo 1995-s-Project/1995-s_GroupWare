6ea212fd963c5f19396f390d07fe2ab8104d78eb
DESKTOP-ET10H8L\hi <dmssl2486@gmail.com>
2024-12-10 09:47:01 +0900

    <241210>구조동물(등록,삭제)

---
+++ semi-project/src/main/java/com/ohgiraffers/semiproject/animals/model/dto/TypeAndBreedAndEmpAndAnimalDTO.java
@@ -0,0 +1,31 @@
+package com.ohgiraffers.semiproject.animals.model.dto;
+
+import lombok.*;
+import org.springframework.format.annotation.DateTimeFormat;
+
+import java.util.Date;
+
+@AllArgsConstructor
+@NoArgsConstructor
+@Getter
+@Setter
+@ToString
+public class TypeAndBreedAndEmpAndAnimalDTO {
+
+
+    private String animalCode;
+    @DateTimeFormat(pattern = "yyyy-MM-dd")
+    private Date rescueDate;
+    private String rescueLocation;
+    private String animalImage;
+    private int age;
+    private String gender;
+    private String animalStatus;
+    private String typeCode;
+    private String breedCode;
+    private String empCode;
+    private TypeDTO typeDTO;
+    private BreedDTO breedDTO;
+    private EmpDTO empDTO;
+
+}
--- semi-project/src/main/java/com/ohgiraffers/semiproject/animals/model/service/AnimalsService.java
+++ semi-project/src/main/java/com/ohgiraffers/semiproject/animals/model/service/AnimalsService.java
@@ -1,7 +1,68 @@
 package com.ohgiraffers.semiproject.animals.model.service;
 
+import com.ohgiraffers.semiproject.animals.model.dao.AnimalsMapper;
+import com.ohgiraffers.semiproject.animals.model.dto.TypeAndBreedAndEmpAndAnimalDTO;
+import com.ohgiraffers.semiproject.animals.model.dto.BreedDTO;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class AnimalsService {
+
+    public final AnimalsMapper animalsMapper;
+
+    @Autowired
+    public AnimalsService(AnimalsMapper animalsMapper){
+        this.animalsMapper = animalsMapper;
+    }
+
+    // 구조동물 전체조회
+    public List<TypeAndBreedAndEmpAndAnimalDTO> getPagedAnimalList(int page, int limit) {
+        int offset = (page - 1) * limit; // 페이지 시작점 계산
+        Map<String, Integer> params = new HashMap<>();
+        params.put("limit", limit);
+        params.put("offset", offset);
+        return animalsMapper.AllList(params);
+    }
+    // 구조동물 전체조회 - 페이징처리
+    public int getTotalAnimalCount() {
+        return animalsMapper.getTotalAnimalCount();
+    }
+
+    // 동물등록
+    @Transactional
+    public void newAnimal(TypeAndBreedAndEmpAndAnimalDTO animalsDTO) {
+        animalsMapper.newAnimal(animalsDTO);
+    }
+    // 동물등록 - 품종 비동기처리
+    public List<BreedDTO> findBreed() {
+        return animalsMapper.findBreed();
+    }
+    // 동물등록 - 동물등록번호 자동입력
+    public String autoAnimalCode() {
+        String lastAnimalCode = animalsMapper.findLastAnimalCode();
+
+        if (lastAnimalCode != null) {
+            int lastNumber = Integer.parseInt(lastAnimalCode.substring(1));  // 'A' 제외한 숫자만 추출
+            int nextNumber = lastNumber + 1;  // 1을 더함
+            return "A" + String.format("%03d", nextNumber);  // 'A'와 3자리 숫자로 반환
+        } else {
+            return "A0010";  // 첫 번째 동물 코드
+        }
+    }
+
+    // 체크박스 선택 후 삭제
+    @Transactional
+    public void deleteBoard(String id) {
+        animalsMapper.deleteBoard(id);
+    }
+
+    
+
+
 }
