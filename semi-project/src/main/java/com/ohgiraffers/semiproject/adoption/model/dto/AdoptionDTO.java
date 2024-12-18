234c2bcad45d07fb1c01d091b1280b74114d6f46
EUNMI <dmssl2486@gmail.com>
2024-12-18 21:03:19 +0900

    [24.12.18]입양진행중,입양완료,입양취소 조회

--- semi-project/src/main/java/com/ohgiraffers/semiproject/adoption/model/dto/AdoptionDTO.java
+++ semi-project/src/main/java/com/ohgiraffers/semiproject/adoption/model/dto/AdoptionDTO.java
@@ -1,5 +1,7 @@
 package com.ohgiraffers.semiproject.adoption.model.dto;
 
+import com.ohgiraffers.semiproject.animals.model.dto.AnimalDTO;
+import com.ohgiraffers.semiproject.animals.model.dto.EmpDTO;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -20,6 +22,7 @@ public class AdoptionDTO {
     private String adoptStartDate;
     @DateTimeFormat(pattern = "yyyy-MM-dd")
     private String adoptEndDate;
-    private String animalCode;
-    private String empCode;
+    private String adoptStatus;
+    private AnimalDTO animalDTO;
+    private EmpDTO empDTO;
 }
