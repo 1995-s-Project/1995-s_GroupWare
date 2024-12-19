c8b0d8b38bed47aa1cc5f44989488e74a5276d6d
DESKTOP-OSFKUPN\hi <rhksgns4@gmail.com>
2024-12-19 20:31:53 +0900

    <24.12.19> 메일조회

--- semi-project/src/main/java/com/ohgiraffers/semiproject/mail/controller/MailController.java
+++ semi-project/src/main/java/com/ohgiraffers/semiproject/mail/controller/MailController.java
@@ -4,13 +4,15 @@ import com.ohgiraffers.semiproject.employee.model.dto.EmployeeJoinListDTO;
 import com.ohgiraffers.semiproject.employee.model.service.EmployeeService;
 import com.ohgiraffers.semiproject.mail.model.dto.MailDTO;
 import com.ohgiraffers.semiproject.mail.model.service.MailService;
+import com.ohgiraffers.semiproject.main.model.dto.UserInfoResponse;
+import com.ohgiraffers.semiproject.main.model.service.UserInfoService;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
-import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.ModelAttribute;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.*;
 
+import java.util.Arrays;
+import java.util.Collections;
 import java.util.Date;
 import java.util.List;
 
@@ -19,46 +21,95 @@ public class MailController {
 
     private final MailService mailService;
     private final EmployeeService employeeService;
+    private final UserInfoService userInfoService;
 
-    public MailController(MailService mailService, EmployeeService employeeService) {
+    public MailController(MailService mailService, EmployeeService employeeService, UserInfoService userInfoService) {
         this.mailService = mailService;
         this.employeeService = employeeService;
+        this.userInfoService = userInfoService;
     }
 
     @GetMapping("/sidemenu/mail")
     public String mailAllSelect(Model model) {
-        List<MailDTO> mail = mailService.mailAllSelect();
 
-        System.out.println("mail = " + mail);
 
-        model.addAttribute("mail", mail);
+        UserInfoResponse userInfo = userInfoService.getUserInfo();
 
-        System.out.println("mail = " + mail);
+        String code = userInfo.getCode();
+
+        List<MailDTO> mail = mailService.mailAllSelect(code);
+
+        model.addAttribute("mailList", mail);
 
         return "sidemenu/mail/mail";
 
     }
+
+    @GetMapping("/mail/sent")
+    public String mailSentSelect(Model model) {
+
+        UserInfoResponse userInfo = userInfoService.getUserInfo();
+
+        String code = userInfo.getCode();
+
+        List<MailDTO> mail = mailService.mailSentSelect(code);
+
+        model.addAttribute("mailList", mail);
+
+        return "sidemenu/mail/mailSent";
+    }
+
+
+    @GetMapping("/mail/details/{emailCode}")
+    public String mailDetail(@PathVariable Integer emailCode, Model model) {
+
+        MailDTO mail = mailService.mailDetail(emailCode);
+
+        model.addAttribute("mail", mail);
+
+        return "sidemenu/mail/mailDetail";
+    }
+
     // 직원정보 보내기
     @GetMapping("api/employee")
     public ResponseEntity<List<EmployeeJoinListDTO>> employee() {
 
         List<EmployeeJoinListDTO> employeeList = employeeService.empAllSelect();
-
+        for (EmployeeJoinListDTO list : employeeList) {
+            System.out.println("list = " + list);
+        }
         return ResponseEntity.ok(employeeList);
     }
 
-//    @GetMapping("/sidemenu/mail/regist")
-//    public void mailRegist(){}
+    @PostMapping("/sidemenu/mail/regist")
+    public String registMail(@RequestParam("recipientId") String recipientId, @RequestParam("recipientName") String recipientName, @ModelAttribute MailDTO mailDTO) {
+
+        // recipientId 값을 mailDTO에 설정
+        mailDTO.setRecipientId(recipientId);
+        mailDTO.setRecipientName(recipientName);
+        System.out.println("쪽지 = " + mailDTO);
+
+        // 나머지 로직
+        UserInfoResponse userInfo = userInfoService.getUserInfo();
+        String code = userInfo.getCode(); // 로그인된 사번
+        Date now = new Date();
+
+        mailDTO.setSendDate(now);
+
+        mailDTO.setSenderId(code); // 로그인한 사용자 사번으로 발신자 설정
+
+        mailService.registMail(mailDTO);
+
+        System.out.println("쪽지쓰기 = " + mailDTO);
+
+        return "redirect:/sidemenu/mail"; // 메인 페이지로 리다이렉트
+    }
+
+//    @GetMapping("/mail/{emailCode}/trash/")
+//    public String mailTrash(@PathVariable Integer emailCode, Model model) {
 //
-//    @PostMapping("/sidemenu/mail/regist")
-//    public String registMail(@ModelAttribute MailDTO mailDTO) {
+//        mailService.mailTrash(emailCode);
 //
-//        Date now = new Date();
-//
-//        mailDTO.setSendDate(now);
-//
-//        mailService.registMail(mailDTO);
-//
-//        return "redirect:/sidemenu/mail";
+//        return "";
 //    }
 }
--- semi-project/src/main/java/com/ohgiraffers/semiproject/mail/model/dto/MailDTO.java
+++ semi-project/src/main/java/com/ohgiraffers/semiproject/mail/model/dto/MailDTO.java
@@ -1,8 +1,8 @@
 package com.ohgiraffers.semiproject.mail.model.dto;
 
-import com.ohgiraffers.semiproject.employee.model.dto.EmployeeDTO;
 import lombok.*;
 import java.util.Date;
+import java.util.List;
 
 @NoArgsConstructor
 @AllArgsConstructor
@@ -11,18 +11,20 @@ import java.util.Date;
 @ToString
 public class MailDTO {
 
-    private String emailCode; // 메일코드
+    private Integer emailCode; // 메일코드
     private String importantEmail; // 중요여부
-    private String emailTitle; // 쪽지제목
-    private String emailFile; // 첨부파일
-    private String emailContent; // 내용
+    private String title; // 쪽지제목
+    private String file; // 첨부파일
+    private String content; // 내용
     private Date sendDate; // 전송일자
     private Date deleteDate; // 삭제일자
-    private String address; // 메일주소
-    private String mailboxNo; // 메일함번호
     private String receptionType; // 수신종류
     private String receptionStatus; // 수신상태
     private String senderId; // 발신자 사번
-    private String recipientId; // 수신자 사번
+    private String recipientId; // 다수의 recipientId를 저장할 리스트
+    private String recipientName; // 다수의 recipientName을 저장할 리스트
+    private EmployeeDTO employeeDTO;
+    private JobDTO jobDTO;
+    private DeptDTO deptDTO;
 
 }
